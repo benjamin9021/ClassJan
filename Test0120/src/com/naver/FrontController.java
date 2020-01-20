daa304ca98b921256434b53ade865ef732675524
benjamin9021 <rkmin9021@gmail.com>
2020-01-20 16:26:13 +0900

    add 20200120

---
+++ Test0120/src/com/naver/FrontController.java
@@ -0,0 +1,53 @@
+package com.naver;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FrontController
+ */
+//@WebServlet("/FrontController")
+public class FrontController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+ 
+    public FrontController() {
+        super();
+       
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String uri = request.getRequestURI();
+		String cPath = request.getContextPath();
+		String what = uri.substring(cPath.length());
+		
+		Command com = null;
+		if (what.equalsIgnoreCase("/list.do")) {
+			com = new ListCommand();
+		}else if (what.equalsIgnoreCase("/login.do")) {
+			com = new LoginCommand();
+		}else if (what.equalsIgnoreCase("/loginui.do")) {
+			com = new LoginUICommand();
+		}
+		if (com != null) {
+			com.execute(request, response);
+		}
+		if (com != null) {
+			CommandAction action = com.execute(request, response);
+			if (action.isRedirect()) {
+				response.sendRedirect(action.getWhere());
+			} else {
+				request.getRequestDispatcher(action.getWhere()).forward(request, response);
+			}
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ Test01202/WebContent/header.jsp
@@ -0,0 +1,6 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+    pageEncoding="EUC-KR"%>
+
+    <header>
+    <h1> header¿‘¥œ¥Ÿ.</h1>
+     </header>
